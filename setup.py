b7567566f350e952b701053e846360b7a1bc3c06
Briggs Baltzell <b3apollo13@gmail.com>
2019-11-15 14:26:01 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="chromia",
+    version="0.1.0",
+    description="Color module for Python",
+    long_description="Provides a variety of utilities for working with color. \nBased on `chroma.js`.",
+    author="Briggs Baltzell",
+    maintainer="Briggs Baltzell",
+    maintainer_email="b3apollo13@gmail.com",
+    url="https://github.com/TheGreatB3/chromia-py",
+)
